618dd92d66333e2fc62de00b0cc9835e110d98a6
Paolo Mottadelli <paolo.moz@gmail.com>
2026-01-07 14:54:19 +0100

    Add info-cards block (session horemmvo, opt 3, iter 3)

---
+++ blocks/info-cards/info-cards.js
@@ -0,0 +1,20 @@
+export default function decorate(block) {
+  block.classList.add('info-cards');
+  
+  [...block.children].forEach((row) => {
+    const cells = [...row.children];
+    row.classList.add('info-card');
+    
+    if (cells[0]) {
+      cells[0].classList.add('info-card-icon');
+    }
+    
+    if (cells[1]) {
+      cells[1].classList.add('info-card-title');
+    }
+    
+    if (cells[2]) {
+      cells[2].classList.add('info-card-description');
+    }
+  });
+}
