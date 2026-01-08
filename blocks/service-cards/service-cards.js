99a722b1d96e78e08bf3227afb246399a33d88d7
Paolo Mottadelli <paolo.moz@gmail.com>
2026-01-08 11:15:20 +0100

    Add service-cards block (session hyun3jhh, opt 1, iter 1)

---
+++ blocks/service-cards/service-cards.js
@@ -0,0 +1,24 @@
+export default function decorate(block) {
+  const rows = [...block.children];
+  
+  rows.forEach((row) => {
+    row.classList.add('card');
+    const cells = [...row.children];
+    
+    if (cells[0]) {
+      cells[0].classList.add('card-image');
+      const img = cells[0].querySelector('img');
+      if (img) {
+        img.loading = 'lazy';
+      }
+    }
+    
+    if (cells[1]) {
+      cells[1].classList.add('card-title');
+    }
+    
+    if (cells[2]) {
+      cells[2].classList.add('card-description');
+    }
+  });
+}
