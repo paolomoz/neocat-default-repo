94d9c19d60cd39b9dc9329374ab23d009f03e0fd
Paolo Mottadelli <paolo.moz@gmail.com>
2026-01-08 08:18:33 +0100

    Add promo-cards block (session bvgp7p2z, opt 3, iter 1)

---
+++ blocks/promo-cards/promo-cards.js
@@ -0,0 +1,22 @@
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    const cells = [...row.children];
+    row.classList.add('promo-card');
+    
+    if (cells[0]) {
+      cells[0].classList.add('promo-card-image');
+    }
+    if (cells[1]) {
+      cells[1].classList.add('promo-card-eyebrow');
+    }
+    if (cells[2]) {
+      cells[2].classList.add('promo-card-heading');
+    }
+    if (cells[3]) {
+      cells[3].classList.add('promo-card-description');
+    }
+    if (cells[4]) {
+      cells[4].classList.add('promo-card-cta');
+    }
+  });
+}
