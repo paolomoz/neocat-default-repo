a1ae65d4292f4c118e43fa4941584e02ae0e11d1
Paolo Mottadelli <paolo.moz@gmail.com>
2026-01-08 10:38:08 +0100

    Add product-cards block (session 4k83pfh2, opt 3, iter 1)

---
+++ blocks/product-cards/product-cards.js
@@ -0,0 +1,18 @@
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    const cells = [...row.children];
+    row.classList.add('card');
+    
+    if (cells[0]) {
+      cells[0].classList.add('card-image');
+    }
+    
+    if (cells[1]) {
+      cells[1].classList.add('card-title');
+    }
+    
+    if (cells[2]) {
+      cells[2].classList.add('card-description');
+    }
+  });
+}
