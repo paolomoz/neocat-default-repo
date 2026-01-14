218230486d71e726dda1e22d282872cd5b56166b
Paolo Mottadelli <paolo.moz@gmail.com>
2026-01-14 16:17:26 +0100

    Add quick-links block (session pxeetyvk, opt 2, iter 1)

---
+++ blocks/quick-links/quick-links.js
@@ -0,0 +1,16 @@
+export default function decorate(block) {
+  const rows = [...block.children];
+  
+  rows.forEach((row) => {
+    row.classList.add('quick-links-item');
+    const cells = [...row.children];
+    
+    if (cells[0]) {
+      cells[0].classList.add('quick-links-icon');
+    }
+    
+    if (cells[1]) {
+      cells[1].classList.add('quick-links-link');
+    }
+  });
+}
