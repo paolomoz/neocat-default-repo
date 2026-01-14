14b0985de2bcf28714765c8cd1e146c04af4a563
Paolo Mottadelli <paolo.moz@gmail.com>
2026-01-14 18:41:55 +0100

    Add news-cards block (session rh2v4cpr, opt 2, iter 1)

---
+++ blocks/news-cards/news-cards.js
@@ -0,0 +1,26 @@
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    const cells = [...row.children];
+    row.classList.add('news-cards-item');
+    
+    if (cells[0]) {
+      cells[0].classList.add('news-cards-image');
+    }
+    
+    if (cells[1]) {
+      cells[1].classList.add('news-cards-title');
+    }
+    
+    if (cells[2]) {
+      cells[2].classList.add('news-cards-date');
+    }
+    
+    if (cells[3]) {
+      cells[3].classList.add('news-cards-excerpt');
+    }
+    
+    if (cells[4]) {
+      cells[4].classList.add('news-cards-link');
+    }
+  });
+}
